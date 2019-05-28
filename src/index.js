a0c510c28685f3a2b632f1640467c2bf2989000f
Rauf Erk <erk.rauf@gmail.com>
2019-05-28 21:06:57 +0300

    28 may 21:06 try ti add Todo Item)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello World!!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app'
+
+ReactDOM.render(<App />, document.getElementById('root'));
